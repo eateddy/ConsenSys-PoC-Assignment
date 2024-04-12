6b347e53fdcb9bdd3a626398330ffaa26be5d1e2
EdwinChan <edwinkhchan@gmail.com>
2024-04-12 15:01:02 +0800

    hardhat and open zeppelin contracts installed

---
+++ frontend/hardhat.config.cjs
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+const { VITE_MM_PRIVATE_KEY, VITE_INFURA_API_KEY } = process.env;
+
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    linea_sepolia: {
+      url: `https://linea-sepolia.infura.io/v3/${VITE_INFURA_API_KEY}`,
+      accounts: [VITE_MM_PRIVATE_KEY],
+    }
+  },
+};
