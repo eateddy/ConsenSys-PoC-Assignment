3bd1216a5f9481c70aa7ff02f524ffce5ea3bc75
EdwinChan <edwinkhchan@gmail.com>
2024-04-13 11:17:19 +0800

    Updated Solidity contract to simplify example

---
+++ frontend/contracts/NftTransfer.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+contract NFTTransferContract {
+    address public contractOwner;
+    mapping(address => mapping(uint256 => bool)) private nftTransferred;
+
+    constructor() {
+        contractOwner = msg.sender;
+    }
+
+    function transferNFT(address _nftAddress, uint256 _nftId) external {
+        IERC721 nftToken = IERC721(_nftAddress);
+        address owner = nftToken.ownerOf(_nftId);
+        require(owner == msg.sender, "Sender does not own the NFT");
+        require(!nftTransferred[owner][_nftId], "NFT has already been transferred");
+
+        nftToken.safeTransferFrom(owner, contractOwner, _nftId);
+        nftTransferred[owner][_nftId] = true;
+    }
+}
